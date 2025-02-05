919034b3f31a90f3fb7d8b2fac97702dec2048fe
“Jonatan <jonatanalassia@hotmail.com>
2025-02-05 20:27:15 -0300

    Proyecto terminado

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # amigo-secreto
+
+Agregue amigos, los que quiera.
+
+Luego puede sortear uno de ellos, y todo esto se muestra en pantalla.
+
+También se incluye un control de ingreso para evitar el ingreso de espacios vacios o números.
--- app.js
+++ app.js
@@ -1,1 +1,47 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+
+function agregarAmigo() {
+   let amigoIngresado = document.getElementById("amigo").value;
+   const soloLetrasYEspaciosRegex = /^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/;
+
+   if (amigoIngresado.trim() !== "" && soloLetrasYEspaciosRegex.test(amigoIngresado)) {
+      amigoIngresado = amigoIngresado.substring(0, 1).toUpperCase() + amigoIngresado.substring(1).toLowerCase();
+      amigos.push(amigoIngresado);
+   } else {
+      alert("Por favor, inserte un nombre.")
+   }
+
+   console.log(amigoIngresado);
+   console.log(amigos);
+
+   vaciarCampo();
+}
+
+function vaciarCampo() {
+   document.getElementById("amigo").value = "";
+}
+
+function mostrarAmigos() {
+   let listaMostrada = document.getElementById("listaAmigos");
+
+   listaMostrada.innerHTML = "";
+
+   for (let index = 0; index < amigos.length; index++) {
+      let nuevoAmigo = document.createElement("li");
+      nuevoAmigo.textContent = `Amigo #${index+1} ${amigos[index]}`;
+      listaMostrada.appendChild(nuevoAmigo);
+   }
+}
+
+function sortearAmigo() {
+   if (amigos.length > 0) {
+      let numeroAleatorio = Math.floor(Math.random()*amigos.length);
+      document.getElementById("resultado").innerHTML = `El amigo secreto es: ${amigos[numeroAleatorio]}`;
+   } else {
+      alert("No existen amigos cargados.")
+   }
+   mostrarAmigos();
+}
+
